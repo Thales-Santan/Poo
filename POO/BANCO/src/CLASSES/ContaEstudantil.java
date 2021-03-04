324e34cc059e837853f4aed26efb0d925497e517
Thales Santana <generationt2h@gmail.com>
2021-03-04 18:28:58 -0300

    Exercicios POO e miniprojeto Banco

---
+++ POO/BANCO/src/CLASSES/ContaEstudantil.java
@@ -0,0 +1,42 @@
+package CLASSES;
+
+public class ContaEstudantil extends Conta {
+	private double limiteEstudantil;
+
+	public ContaEstudantil(int numero, String cpf, double limiteEstudantil, boolean ativa) {
+		super(numero, cpf, ativa);
+		this.limiteEstudantil = limiteEstudantil;
+	}
+
+	public double getLimiteEstudantil() {
+		return limiteEstudantil;
+	}
+	public void setLimiteEstudantil(double limiteEstudantil) {
+		this.limiteEstudantil = limiteEstudantil;
+	}
+	//metodo
+	public void usarEstudantil(double limite) {
+		if(limite <= 5000) {
+			saldo = 5000;
+		}
+		if(limite > 0) {
+			saldo = 5000;
+		}
+		if(limite < 0) {
+			saldo = 0;
+			System.out.println("Opção invalida!!!");
+		}
+		if(limite > 5000) {
+			saldo = 0;
+		}
+	}
+		
+		
+	
+	
+	
+	
+
+}
+
+
---
+++ POO/src/Funcionario.java
@@ -0,0 +1,56 @@
+
+public class Funcionario {
+	//atributos
+	private String nome;
+	private int matricula;
+	protected int horasTrabalhadas;
+	protected double valorPorHora;
+	//construtores -Como eu faço o objeto funcionario no programa pessoal
+	public Funcionario(String nome, int matricula, int horasTrabalhadas, double valorHora) {
+		super();
+		this.nome = nome;
+		this.matricula = matricula;
+		this.horasTrabalhadas = horasTrabalhadas;
+		this.valorPorHora = valorHora;
+	}
+	//encapsulamento -Como eu mexo nas coisas se tá tudo travado
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public int getHorasTrabalhadas() {
+		return horasTrabalhadas;
+	}
+
+	public void setHorasTrabalhadas(int horasTrabalhadas) {
+		this.horasTrabalhadas = horasTrabalhadas;
+	}
+
+	public double getValorHora() {
+		return valorPorHora;
+	}
+
+	public void setValorHora(double valorHora) {
+		this.valorPorHora = valorHora;
+	}
+	//metodo -tudo que a classe faz
+	public double salario() {
+		return (horasTrabalhadas * valorPorHora);
+	}
+	//sobrecarga de metodo
+	public double salario(double imposto) {
+		return (horasTrabalhadas * valorPorHora) -((horasTrabalhadas * valorPorHora)*imposto) ;
+	}
+}
